3fadc67c7d50b833d61889abd86d7c9c17e37e6c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-10 19:09:33 +0000

    feat: Add Collection component  and styles

---
+++ components/collection/Collection.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import NavLink from "@components/header/link/NavLink";
+import classes from "./collection.module.scss";
+
+export default function Collection({ title, date, img, alt, darkMode }) {
+  return (
+    <section className={classes.collection}>
+      <article className={classes.article}>
+        <p className={classes.date}>{date}</p>
+        <div>
+          <h3 className={classes.heading}>{title}</h3>
+
+          <NavLink className={`${darkMode ? "" : classes.light}`} href="/">
+            Menu
+          </NavLink>
+        </div>
+        <Image
+          className={`${classes.img} ${classes.imgLeft}`}
+          width={200}
+          height={400}
+          src={img}
+          alt={alt}
+        />
+        <Image
+          className={`${classes.img} ${classes.imgRight}`}
+          width={200}
+          height={400}
+          src={img}
+          alt={alt}
+        />
+      </article>
+    </section>
+  );
+}
