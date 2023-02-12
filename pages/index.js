a52a96819784de4017dedca04c2c1665b02c4820
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-12 18:44:32 +0000

    chore: Pass more props down

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Scroll from "@components/scroll/Scroll";
+// import Scroll from "@components/scroll/Scroll";
 import Hero from "@components/hero/Hero";
 import About from "@components/about/About";
 import Collection from "@components/collection/Collection";
 import Body from "@components/body/Body";
 import Footer from "@components/footer/Footer";
+import { caseImages } from "../utils/utils";
 
 export default function Home() {
   return (
@@ -14,8 +15,22 @@ export default function Home() {
       </div>
       <main style={{ overflow: "hidden" }}>
         <About />
-        <Collection title="Follow The Tracks" date="September 2019" />
-        <Collection title="Follow The Tracks" date="September 2019" />
+        <Collection
+          title="Follow The Tracks"
+          date="September 2019"
+          place="Mongolia"
+          img1={caseImages.case1}
+          img2={caseImages.case2}
+          alt="Mongolia trip photos"
+        />
+        <Collection
+          title="Cayuga Collection"
+          date="July 2021"
+          place="Costa Rica"
+          img1={caseImages.case3}
+          img2={caseImages.case4}
+          alt="Cayuga Collection trip photos"
+        />
       </main>
       <div className="container">
         <Footer />
