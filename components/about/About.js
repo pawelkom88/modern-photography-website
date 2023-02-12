0f01d3d54cca3bcc483a4d2afd3bd4cc44c0302d
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-12 18:13:45 +0000

    chore; implement intersection observer

--- components/about/About.js
+++ components/about/About.js
@@ -1,25 +1,45 @@
-import Heading from "./heading/Heading";
 import React from "react";
+import { useInView } from "react-intersection-observer";
+import Heading from "./heading/Heading";
+import NavLink from "@components/header/link/NavLink";
 import { aboutImages } from "../../utils/utils.js";
 import classes from "./about.module.scss";
 
 export default function About() {
+  const { ref, inView } = useInView({ delay: 400 });
+
   return (
     <section className={classes.about}>
       <div className={classes["heading-wrapper"]}>
         <Heading text="An" img={aboutImages.mini1}>
           ordinary
         </Heading>
-        <Heading text="town" img={aboutImages.mini1}>
+        <Heading text="town" img={aboutImages.mini2}>
           where it all
         </Heading>
-        <Heading text="began..." img={aboutImages.mini1}></Heading>
+        <Heading size="big" text="began..." img={aboutImages.mini3}></Heading>
       </div>
-      <p className={classes["about-paragraph"]}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptas explicabo
-        temporibus perferendis velit facere quod neque eos itaque odit. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Labore, ratione?
+      <p ref={ref} className={classes["about-paragraph"]}>
+        <ul className={classes.overflow}>
+          <li className={`${inView ? classes.fadeIn : ""}`}>
+            Giulia Gartner is an outdoor, travel, and commercial photographer, filmmaker, and
+            storyteller
+          </li>
+          <li className={`${inView ? classes.fadeIn : ""}`}>
+            from the Dolomites in northern Italy. Her work focuses on capturing wild and rugged
+          </li>
+          <li className={`${inView ? classes.fadeIn : ""}`}>
+            landscapes with a vivid color palette and dreamy elements. Her love for photography has
+          </li>
+          <li className={`${inView ? classes.fadeIn : ""}`}>
+            spilled over into filmmaking which is one of the main creative outlets she pursues
+            today.
+          </li>
+        </ul>
       </p>
+      <NavLink className={classes.btn} href="/">
+        Read my story
+      </NavLink>
     </section>
   );
 }
