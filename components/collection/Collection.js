defbf3de5da48c53e14e37adf4af6f227a487e91
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-12 18:43:30 +0000

    chore: Pass more props

--- components/collection/Collection.js
+++ components/collection/Collection.js
@@ -3,12 +3,11 @@ import useMatchMedia from "@hooks/useMatchMedia";
 import { ThemeContext } from "context/ThemeMode";
 import Image from "next/image";
 import NavLink from "@components/header/link/NavLink";
-import { collection } from "../../utils/utils";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import classes from "./collection.module.scss";
 
-export default function Collection({ title, date, alt }) {
+export default function Collection({ title, date, place, img1, img2, alt }) {
   const { matches } = useMatchMedia("(max-width: 800px)");
 
   const { darkMode } = React.useContext(ThemeContext);
@@ -30,14 +29,12 @@ export default function Collection({ title, date, alt }) {
       el1,
       { x: -10, rotate: -5 },
       {
-        x: matches ? -300 : -800,
+        x: matches ? -320 : -800,
         scrollTrigger: {
           trigger: triggerEl,
-          markers: true,
           start: "top 50%",
           end: "+=100 50%",
           scrub: 3,
-          // pin: true,
         },
       }
     );
@@ -45,25 +42,34 @@ export default function Collection({ title, date, alt }) {
       el2,
       { x: 10, rotate: 5 },
       {
-        x: matches ? 300 : 800,
+        x: matches ? 320 : 800,
         scrollTrigger: {
           trigger: triggerEl,
           start: "top 50%",
           end: "+=100 50%",
           scrub: 3,
-          // pin: true,
         },
       }
     );
-
-    // return () => {
-    // };
-  }, []);
+  }, [matches]);
 
   return (
     <section ref={triggerRef} className={classes.collection}>
       <article className={classes.article}>
-        <p className={classes.date}>{date}</p>
+        <div className={classes.top}>
+          <p className={classes.date}>{date}</p>
+          <svg
+            width="16"
+            height="10"
+            viewBox="0 0 16 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M16 4L-8.26528e-07 8L-4.76837e-07 -6.99382e-07L16 4Z"
+              fill="currentcolor"></path>
+          </svg>
+          <p className={classes.date}>{place}</p>
+        </div>
         <div>
           <h3 className={classes.heading}>{title}</h3>
           <NavLink className={`${classes.btn} ${darkMode ? "" : classes.light}`} href="/">
@@ -75,15 +81,15 @@ export default function Collection({ title, date, alt }) {
           className={`${classes.img} ${classes.imgLeft}`}
           width={rensponsivnessW}
           height={rensponsivnessH}
-          src={collection.mini1}
-          alt="asd"
+          src={img1}
+          alt={alt}
         />
         <Image
           ref={ref2}
           className={`${classes.img} ${classes.imgRight}`}
           width={rensponsivnessW}
           height={rensponsivnessH}
-          src={collection.mini2}
+          src={img2}
           alt={alt}
         />
       </article>
